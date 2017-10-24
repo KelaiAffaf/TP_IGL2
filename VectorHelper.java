254ee272ae619d2de3bfbcd5b943af8c799090bd
NabilWW <fn_sefta@esi.dz>
2017-10-24 19:20:37 +0200

    chgt effectué

--- VectorHelper.java
+++ VectorHelper.java
@@ -39,4 +39,48 @@ public class VectorHelper {
             System.out.println("V");
         }
     }
+        public int [] sortV (){
+        int [] tmpV = new int[size];
+        int tmp ;
+        boolean bool = false;
+        tmpV=vector;
+        while (!bool){
+            for (int i=0 ;i<size;i++)
+            {
+                if(tmpV[i]>tmpV[i+1]){
+                    tmp = tmpV[i];
+                    tmpV[i] = tmpV[i+1];
+                    tmpV[i+1] = tmp;
+
+                }
+                else {
+                    bool = true;
+                }
+            }
+        }
+        return tmpV;
+    }
+
+ public int [] sommeVectors (int [] vecteur, int size)
+{   
+    int [] tmp = new int [this.size];
+    if (size != this.size) throw new DiffrentSizeException (){
+        for (int i = 0,i<this.size;i++){
+            tmp [i] = this.vector[i] + vecteur[i];
+        }
+    }
+    return tmp;
+
+}
+public int [] InversingV ()
+{
+    int [] tmp = new int [this.size];
+    int j = 0;
+    for (int i = this.size-1;i>=0;i++)
+    {
+        tmp [j] = this.vector[size];
+        j++;
+    }
+    return tmp;
+}
 }
