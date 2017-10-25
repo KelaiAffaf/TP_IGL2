3a7964a9aebc733045ed65b9a0280c01e08cddb6
KelaiAffaf <32898671+KelaiAffaf@users.noreply.github.com>
2017-10-25 13:01:43 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,20 @@ public class Main {
         
         int vector[] = {51, 24, 19, 5, 37, 76, 61, 99, 101, 36};
 
-        VectorHelper v = new VectorHelper();
+         VectorHelper v = new VectorHelper();
+        System.out.println(" le min et le max ");
         v.Max_Min();
         System.out.println("***********************************************************");
         System.out.println("multiplier tous les éléments par 6 apres les diviseer par 2");
-
         v.formule();
+        System.out.println("***********************************************************");
+        System.out.println(" Inverser le tableau  ");
+        v.InversingV();
+        System.out.println("***********************************************************");
+        System.out.println(" trie du tableau   ");
+        v.sortV();
+        System.out.println("***********************************************************");
+
     }
 }
 
